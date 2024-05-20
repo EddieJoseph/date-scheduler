79d22fad799487dd4f7a4201eabc6709a8f12766
Eddie Joseph <eddie.joseph@windowslive.com>
2024-05-20 10:39:53 +0200

    improved type spread evaluator

--- type_spread_evaluator.py
+++ type_spread_evaluator.py
@@ -9,24 +9,26 @@ class TypeSpreadEvaluator(Evaluator):
     def evaluate(self, candidate):
         types = super().get_types(candidate)
         excluded_types =["ST","WTA","SAN","HY","B","ASSITST","IFA","MS","KS"]
+        types = [type for type in types if type not in excluded_types]
+        result = 1
 
         for type in types:
-            if type not in excluded_types:
-                type_candidates = candidate[candidate['type'] == type]
-                for comp in ['RB', 'KB', 'GB']:
-                    type_comp_candidates = type_candidates[type_candidates[comp.lower()] == True]
-                    if len(type_comp_candidates) > 1:
-                        dates = type_comp_candidates['date']
-                        days_between = dates.diff()
-                        days_between = days_between.fillna(365-dates.values[-1]+dates.values[0], inplace=False)
-                        result = result * (1/(1+np.sqrt(days_between.var()/10000)))
-                no_type_comp_candidates = type_candidates[(type_candidates['gb'] == False) &
-                                                          (type_candidates['kb'] == False) &
-                                                          (type_candidates['rb'] == False)]
-
-                if len(no_type_comp_candidates) > 1:
-                    dates = no_type_comp_candidates['date']
+            type_candidates = candidate[candidate['type'] == type]
+            for comp in ['RB', 'KB', 'GB']:
+                type_comp_candidates = type_candidates[type_candidates[comp.lower()] == True]
+                if len(type_comp_candidates) > 1:
+                    dates = type_comp_candidates['date']
                     days_between = dates.diff()
-                    days_between = days_between.fillna(365 - dates.values[-1] + dates.values[0], inplace=False)
-                    result = result * (1 / (1 + np.sqrt(days_between.var() / 10000)))
+                    days_between = days_between.fillna(365-dates.values[-1]+dates.values[0], inplace=False)
+                    result = result * (1/(1+np.sqrt(days_between.var()/10000)))
+            no_type_comp_candidates = type_candidates[(type_candidates['gb'] == False) &
+                                                      (type_candidates['kb'] == False) &
+                                                      (type_candidates['rb'] == False)]
+
+            if len(no_type_comp_candidates) > 1:
+                dates = no_type_comp_candidates['date']
+                days_between = dates.diff()
+                days_between = days_between.fillna(365 - dates.values[-1] + dates.values[0], inplace=False)
+                result = result * (1 / (1 + np.sqrt(days_between.var() / 10000)))
         return result
+
